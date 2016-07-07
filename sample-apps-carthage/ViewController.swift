cb26739d117ea4047978d2eb8e9c331e01bca739
Krisztián Gödrei <godrekrisztian@gmil.com>
2016-07-07 16:36:37 +0200

    sample project

---
+++ sample-apps-carthage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sample-apps-carthage
+//
+//  Created by Krisztian Goedrei on 07/07/16.
+//  Copyright © 2016 bitrise. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
